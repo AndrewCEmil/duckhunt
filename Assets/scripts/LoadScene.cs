27c2c8b847814a846ffd661ea6c7a9d8f707867d
ace <ace@Andrews-MacBook-Pro.local>
2016-04-23 00:20:25 -0400

    Got initial menu working

---
+++ Assets/scripts/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	public void LoadMain() {
+		SceneManager.LoadScene("Main");
+	}
+}
